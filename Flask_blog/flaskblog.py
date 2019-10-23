9add6cc9c5573187940872137169e3ffc2bda337
henry morrin <henry.morrin@iii.com>
2019-10-23 16:20:14 +0100

    Using jinja 2 expressions to diplay variables into html

--- Flask_blog/flaskblog.py
+++ Flask_blog/flaskblog.py
@@ -1,15 +1,32 @@
 from flask import Flask, render_template
 app=Flask(__name__)
 
+posts = [
+    {
+     'author': 'henry morrin',
+     'title': 'my first post',
+     'content': "First content Post",
+     'date_posted':'April 22nd 2018'
+    },
+    {
+     'author': 'alan morrin',
+     'title': 'my alan post',
+     'content': 'alan content Post',
+     'date_posted': 'April 14th 2019'
+    },
+
+
+]
+
 
 @app.route("/")
 @app.route("/home")
 def hello():
-    return render_template("home.html")
+    return render_template("home.html", posts=posts)
 
 @app.route("/about")
 def about():
-    return "<h1>  This is about me </h1> "
+    return render_template("about.html", title="about")
 
 if __name__ == '__main__':
     app.run(debug=True)
