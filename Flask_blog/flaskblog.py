7db65d0c662429369dd96e2c4f514ba54cf65a16
henry morrin <henry.morrin@iii.com>
2019-10-23 15:59:07 +0100

    Working simple Flask routes and template

--- Flask_blog/flaskblog.py
+++ Flask_blog/flaskblog.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 app=Flask(__name__)
 
 
 @app.route("/")
+@app.route("/home")
 def hello():
-    return "<h1> Hello World - This is the homepage </h1> "
+    return render_template("home.html")
+
+@app.route("/about")
+def about():
+    return "<h1>  This is about me </h1> "
+
+if __name__ == '__main__':
+    app.run(debug=True)
